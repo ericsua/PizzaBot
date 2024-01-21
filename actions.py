a14698bc9431a60f81c4c6b25e159561a990dea9
Eric Suardi <33501092+ericsua@users.noreply.github.com>
2024-01-21 22:40:33 +0100

    WIP synonyms

--- actions.py
+++ actions.py
@@ -17,7 +17,21 @@ from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
+class ActionConfirmPizzas(Action):
+	def name(self):
+		return 'action_confirm_pizzas'
 
+	def run(self, dispatcher, tracker, domain):
+		pizza_size = tracker.get_slot("pizza_size")
+		pizza_type = tracker.get_slot("pizza_type")
+		pizza_amount = tracker.get_slot("pizza_amount")
+		pizza_sliced = tracker.get_slot("pizza_sliced")
+		order_details = ""
+		for amount, type, size, sliced in zip(pizza_amount, pizza_type, pizza_size, pizza_sliced):
+			order_details += f"{amount} {size} {type} " + ("sliced" if sliced == "yes" else "not sliced") + ", "
+		order_details =  str(pizza_amount + " "+pizza_type + " is of "+pizza_size + " and sliced: "+pizza_sliced)
+		dispatcher.utter_message(text="So you want to order "+order_details+" is that correct?")
+		return[]
 
 class ActionChangeOrder(Action):
 	def name(self):
@@ -83,17 +97,18 @@ class ActionGetPizzaTypes(Action):
 	def run(self, dispatcher, tracker, domain):
      
 		#pizza_types = ""
-		pizza_type = tracker.get_slot("pizza_types")
-		if pizza_type is None:
-			pizza_types = "Carnivora, Pepperoni, Trentina, Ortolana, Caprese"
-		elif pizza_type.lower() == "meat":
-			pizza_types = "Carnivora, Pepperoni, Trentina"
-		elif pizza_type.lower() == "vegetarian":
-			pizza_types = "Ortolana, Caprese"
+		pizza_category = tracker.get_slot("pizza_category")
+		if pizza_category is None:
+			#pizza_types = "Funghi, Hawaii, Margherita, Pepperoni, Vegetarian"
+			dispatcher.utter_message(response="utter_inform_pizza_types")
+		elif pizza_category.lower() == "meat":
+			dispatcher.utter_message(response="utter_inform_pizza_types_meat")
+		elif pizza_category.lower() in ["vegetarian", "vegan", "vegetable", "veggie"]:
+			dispatcher.utter_message(response="utter_inform_pizza_types_vegetarian")
 		else:
-			pizza_types = "Carnivora, Pepperoni, Trentina, Ortolana, Caprese"
+			dispatcher.utter_message(response="utter_inform_pizza_types")
 
-		return[SlotSet("pizza_types", pizza_types)]
+		return [SlotSet("pizza_category", None)]
 
 
 # for a generic slot validation please refer to https://rasa.com/docs/action-server/validation-action/
@@ -107,7 +122,7 @@ class ValidatePizzaOrderForm(FormValidationAction):
 	def pizza_db() -> List[Text]:
 		"""Database of supported cuisines"""
 
-		return ["hawaii", "funghi", "french"]
+		return ["hawaii", "fungi", "french"]
 
 	def validate_pizza_type(
 		self,
@@ -118,6 +133,7 @@ class ValidatePizzaOrderForm(FormValidationAction):
 	) -> Dict[Text, Any]:
 		"""Validate cuisine value."""
 
+		#print("pizza type value", slot_value, tracker.get_slot("pizza_type"))
 		if isinstance(slot_value, str):
 			if slot_value.lower() in self.pizza_db():
 				# validation succeeded, set the value of the "cuisine" slot to value
@@ -132,3 +148,83 @@ class ValidatePizzaOrderForm(FormValidationAction):
 				# validation failed, set this slot to None so that the
 				# user will be asked for the slot again
 				return {"pizza_type": None}
+
+	def validate_pizza_amount(
+		self,
+		slot_value: Any,
+		dispatcher: CollectingDispatcher,
+		tracker: Tracker,
+		domain: DomainDict,
+	) -> Dict[Text, Any]:
+		"""Validate cuisine value."""
+
+		if isinstance(slot_value, str):
+			if slot_value in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]:
+				# validation succeeded, set the value of the "cuisine" slot to value
+				return {"pizza_amount": slot_value}
+			else:
+				dispatcher.utter_message(text="Please tell me a valid number")
+				return {"pizza_amount": None}
+		elif isinstance(slot_value, list):
+			if len(slot_value) > 0:
+				concatenated_slot = ", ".join(slot_value)
+				return {"pizza_amount": concatenated_slot}
+			else:
+				# validation failed, set this slot to None so that the
+				# user will be asked for the slot again
+				dispatcher.utter_message(text="Please tell me a valid number")
+				return {"pizza_amount": None}
+
+	def validate_pizza_size(
+		self,
+		slot_value: Any,
+		dispatcher: CollectingDispatcher,
+		tracker: Tracker,
+		domain: DomainDict,
+	) -> Dict[Text, Any]:
+		"""Validate cuisine value."""
+
+		if isinstance(slot_value, str):
+			if slot_value.lower() in ["baby", "small", "medium", "standard", "large", "extra large"]:
+				# validation succeeded, set the value of the "cuisine" slot to value
+				return {"pizza_size": slot_value}
+			else:
+				dispatcher.utter_message(text="Please tell me a valid size. We have baby, small, medium, standard, large, extra large")
+				return {"pizza_size": None}
+		elif isinstance(slot_value, list):
+			if len(slot_value) > 0:
+				concatenated_slot = ", ".join(slot_value)
+				return {"pizza_size": concatenated_slot}
+			else:
+				# validation failed, set this slot to None so that the
+				# user will be asked for the slot again
+				dispatcher.utter_message(text="Please tell me a valid size. We have baby, small, medium, standard, large, extra large")
+				return {"pizza_size": None}
+
+	def validate_pizza_sliced(
+		self,
+		slot_value: Any,
+		dispatcher: CollectingDispatcher,
+		tracker: Tracker,
+		domain: DomainDict,
+	) -> Dict[Text, Any]:
+		"""Validate cuisine value."""
+
+		if isinstance(slot_value, str):
+			if tracker.get_intent_of_latest_message() == "response_positive": #slot_value.lower() in ["yes", "no"]:
+				# validation succeeded, set the value of the "cuisine" slot to value
+				return {"pizza_sliced": True}
+			elif tracker.get_intent_of_latest_message() == "response_negative":
+				return {"pizza_sliced": False}
+			else:
+				#dispatcher.utter_message(text="Please tell me if you want the pizza sliced or not, with a yes or a no")
+				return {"pizza_sliced": None}
+		elif isinstance(slot_value, list):
+			if len(slot_value) > 0:
+				concatenated_slot = ", ".join(slot_value)
+				return {"pizza_sliced": concatenated_slot}
+			else:
+				# validation failed, set this slot to None so that the
+				# user will be asked for the slot again
+				#dispatcher.utter_message(text="Please tell me yes or no")
+				return {"pizza_sliced": None}
